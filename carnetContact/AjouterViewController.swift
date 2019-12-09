20f60ddc52645fe267445c751124997ca51a9fdd
Diana <nava_diana95@hotmail.com>
2019-12-09 02:17:34 +0100

    Funtionlité de la camera et library de photos

--- carnetContact/AjouterViewController.swift
+++ carnetContact/AjouterViewController.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
-    
+class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+ 
     
     
     @IBOutlet weak var InputFamil: UITextField!
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var image: UIImageView!
-    var famillePicker: [String] = [String]()
+ 
     
     let imagePicker: UIImagePickerController = UIImagePickerController()
     
@@ -26,14 +26,63 @@ class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, 
         self.dismiss(animated: true, completion: nil)
     }
 
+    @IBOutlet weak var Relationfamilial: UITextField!
+    @IBOutlet weak var SelectRelation: UIPickerView!
+    
+    var family = ["Pere","Mere","Soeur"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        
-        picker.delegate = self
-        picker.dataSource = self
-        famillePicker = ["Pere","Mere","Soeur","Frere","Coisin","Uncle"]
- 
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+         return 1
+     }
+     
+     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return family.count
+     }
+    
+    func pickerView(_pickerView:UIPickerView, titleForRow row:Int, forComponent component: Int)-> String? {
+        self.view.endEditing(true)
+        return family[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.InputFamil.text = self.family[row]
+        self.picker.isHidden = true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == self.InputFamil{
+            self.picker.isHidden=false
+            textField.endEditing(true)
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func selectPoto (_sender: Any){
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .photoLibrary
+                present(imagePicker, animated : true,  completion: nil)
+        }
     }
     
     
@@ -51,7 +100,10 @@ class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedPicture: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             image.image = selectedPicture
-            UIImageWriteToSavedPhotosAlbum(selectedPicture, nil, nil, nil)
+            if(imagePicker.sourceType == .camera){
+                UIImageWriteToSavedPhotosAlbum(selectedPicture, nil, nil, nil)
+            }
+            
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
@@ -59,20 +111,7 @@ class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return famillePicker.count
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return famillePicker[row]
-    }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        InputFamil.text = famillePicker[row]
-    }
-    
     
     
     

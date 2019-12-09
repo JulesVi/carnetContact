ffc45d6eb3c3dd44ee284d38fde41c98472aadec
IUT1 <iut>
2019-12-09 10:12:39 +0100

    Page de register contact visual funtionalities

--- carnetContact/AjouterViewController.swift
+++ carnetContact/AjouterViewController.swift
@@ -10,18 +10,16 @@ import UIKit
 
 class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
  
-    
+    var family: [String] = [String]()
+    var group: [String] = [String]()
     
     @IBOutlet weak var InputFamil: UITextField!
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var image: UIImageView!
- 
+    @IBOutlet weak var pickerGroup: UIPickerView!
+    @IBOutlet weak var InputGroup: UITextField!
     
     let imagePicker: UIImagePickerController = UIImagePickerController()
-    
-    
-    
-    
     @IBAction func closeAjouter(){
         self.dismiss(animated: true, completion: nil)
     }
@@ -29,11 +27,17 @@ class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var Relationfamilial: UITextField!
     @IBOutlet weak var SelectRelation: UIPickerView!
     
-    var family = ["Pere","Mere","Soeur"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        
+        picker.delegate = self
+        picker.dataSource = self
+        family = ["Pere","Mere","Soeur"]
+        
+        pickerGroup.delegate = self
+        pickerGroup.dataSource = self
+        group = ["Group 1","Group 2","Group 3"]
     }
     
     
@@ -42,40 +46,46 @@ class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, 
      }
      
      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return family.count
+        var donnee : Int = family.count
+        
+        if pickerView == pickerGroup {
+            donnee = self.group.count
+        }
+        return donnee
      }
     
     func pickerView(_pickerView:UIPickerView, titleForRow row:Int, forComponent component: Int)-> String? {
-        self.view.endEditing(true)
-        return family[row]
+        if _pickerView == picker {
+            return family[row]
+        } else if _pickerView == pickerGroup{
+            return group[row]
+        }
+        return ""
     }
     
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.InputFamil.text = self.family[row]
         self.picker.isHidden = true
+        self.pickerGroup.isHidden = true
+        if pickerView == picker {
+            self.InputFamil.text = self.family[row]
+            
+        } else if pickerView == pickerGroup {
+            self.InputGroup.text = self.group[row]
+            
+        }
+        
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         if textField == self.InputFamil{
             self.picker.isHidden=false
             textField.endEditing(true)
+        } else if textField == self.InputGroup{
+            self.pickerGroup.isHidden=false
+            textField.endEditing(true)
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     @IBAction func selectPoto (_sender: Any){
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
@@ -84,7 +94,6 @@ class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, 
                 present(imagePicker, animated : true,  completion: nil)
         }
     }
-    
     
     @IBAction func PrendrePhoto(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.camera){
@@ -103,7 +112,6 @@ class AjouterViewController: UIViewController, UIImagePickerControllerDelegate, 
             if(imagePicker.sourceType == .camera){
                 UIImageWriteToSavedPhotosAlbum(selectedPicture, nil, nil, nil)
             }
-            
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
